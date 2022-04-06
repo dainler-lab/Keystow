76ec25ca3d1ca77aa998393a57cb5561201ffce9
Dainler <dainler.ms@gmail.com>
2022-04-06 16:02:03 -0400

    Início do projeto: Hello Word!

---
+++ keystow/src/main/java/br/com/keystow/KeystowApplication.java
@@ -0,0 +1,13 @@
+package br.com.keystow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeystowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeystowApplication.class, args);
+	}
+
+}
---
+++ keystow/src/test/java/br/com/keystow/tests/KeystowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.keystow.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeystowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
