4d68703ee176b5464b7340563b5dc2ef273329d5
Dainler <dainler.ms@gmail.com>
2022-05-06 19:37:51 -0400

    Revert "Criando classes model, repository e controller rest de usuários"
    This reverts commit fd96cd0498b48fff065b6d73c91166d8807bf7f8.

--- keystow/src/main/java/br/com/keystow/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.keystow.repository;
-
-import br.com.keystow.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
