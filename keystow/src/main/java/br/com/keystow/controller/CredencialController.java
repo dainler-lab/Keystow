ebcaf590ff0fe2bcd2c23a9050f601293c8a12df
Dainler <dainler.ms@gmail.com>
2022-05-11 20:21:16 -0400

    Organizando imports

--- keystow/src/main/java/br/com/keystow/controller/CredencialController.java
+++ keystow/src/main/java/br/com/keystow/controller/CredencialController.java
@@ -1,15 +1,15 @@
 package br.com.keystow.controller;
 
-import br.com.keystow.model.Credencial;
-import br.com.keystow.repository.CredencialRepository;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.keystow.model.Credencial;
+import br.com.keystow.repository.CredencialRepository;
 import lombok.AllArgsConstructor;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/credenciais")
@@ -18,10 +18,8 @@ public class CredencialController {
 
 	private final CredencialRepository credencialRepository;
 
-	// @RequestMapping(method = RequestMethod.GET)
-	@GetMapping
+	@GetMapping // @RequestMapping(method = RequestMethod.GET)
 	public @ResponseBody List<Credencial> list() {
 		return credencialRepository.findAll();
 	}
-
 }
